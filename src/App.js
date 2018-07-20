1af9d459388a7df196155cf0ce2e2d1d71dc949e
Reinier <39220977+RvDstudio@users.noreply.github.com>
2018-07-20 22:54:26 +0200

    Add files via upload
    Start of the RvReact boilerplate with gulp for scss

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/headerComponent/header';
+
+import './Assets/css/default.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import ReactDom from 'react-dom';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+          <div className="logo">
+              RvDesignStudio
+          </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
