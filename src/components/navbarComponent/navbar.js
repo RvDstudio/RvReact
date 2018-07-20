04d59d3d0fd524834113d4641f06cc5152b77f43
Reinier <39220977+RvDstudio@users.noreply.github.com>
2018-07-21 00:21:14 +0200

    Add files via upload

---
+++ src/components/navbarComponent/navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import ReactDom from 'react-dom';
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+          <div className="logo">
+              RvDesignStudio
+          </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
