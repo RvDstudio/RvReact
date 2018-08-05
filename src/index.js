1704cc0f273e7988ede686c64b663278026c7758
RvDstudio <rvdstudio@gmail.com>
2018-08-05 23:18:05 +0200

    Added reactstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Import Core Bootstrap 4
+import "bootstrap/dist/css/bootstrap.min.css";
+
 ReactDOM.render(
     <App />, 
     document.getElementById('root')
